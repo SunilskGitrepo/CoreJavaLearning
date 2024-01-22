aa43995a7da6c1564608458dc31e5a1913ff42e5
SunilGitCode1 <Sunilskofficial@gmail.com>
2024-01-23 01:02:26 +0530

    Calculator grade code

--- src/srcJAN22/LAB002.java
+++ src/srcJAN22/LAB002.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class LAB002 {
     public static void main(String[] args) {
          Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your score");
+        System.out.println("Enter your score:");
         int score = sc.nextInt();
          if(score >= 90 && score<=100){
              System.out.println("Grade A");
